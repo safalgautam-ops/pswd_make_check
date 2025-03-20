2f642882a6fa6ada412fa850ad7214f98b9f712d
safalgautam-ops <gtmsafal09@gmail.com>
2025-03-20 13:42:10 +0545

    fully functional and update code

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import re
 import secrets
 import string
+import os
 
 #function for finding strength
 def check_pswd_strength(password):
@@ -43,20 +44,39 @@ def password_generator(l):
     pwd = "".join(secrets.choice(string.ascii_letters+string.digits) for i in range(l))
     return pwd
 
+#file path for storage
+storage_File = "password.txt"
+
 #function to store password 
+def pwd_saver(service,password):
+    with open(storage_File,"a") as file:
+        file.write(f"Service:{service} | Password:{password} \n")
+    print("Saved Successfully!")
+
 
 #taking user input
 def main():
+    user_input = int(input("Which of the service you want to use? \n 1. Generating Password \n 2. Saving Password \n 3. Checking Password Strength \n :"))
+    if (user_input == 3):
     #password checker
-    password = input("Enter your password: ")
-    strength = check_pswd_strength(password)
-    print (f"Your password strength is {strength}")
+        password = input("Enter your password: ")
+        strength = check_pswd_strength(password)
+        print (f"Your password strength is {strength}")
 
+    elif (user_input == 1):
     #password generator
-    lengthof_pwd = int(input("Enter the length of password you want to generate of"))
-    generated_pwd = password_generator(lengthof_pwd)
-    print(f"Your password is {generated_pwd}")
-
+        lengthof_pwd = int(input("Enter the length of password you want to generate of: "))
+        generated_pwd = password_generator(lengthof_pwd)
+        print(f"Your password is {generated_pwd}")
+    
+    elif (user_input == 2):
+    #password storage
+        service = input("Enter the service name of password: ")
+        pwd = input("Enter the password to save")
+        pwd_saver(service,pwd)
+    
+    else:
+        print("Please Enter Correct Number")
 
 
 #this runs when executed directly
